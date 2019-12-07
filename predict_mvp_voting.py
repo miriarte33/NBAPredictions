8f72cf06fabca0eecae8c5b5d5df51fc474f24f6
Manrique Iriarte <quique_iriarte@ultimatesoftware.com>
2019-12-07 15:49:15 -0500

    beginning preprocessing step

--- predict_mvp_voting.py
+++ predict_mvp_voting.py
@@ -1,8 +1,18 @@
 import get_stats
+import pandas
 
 
 def main():
     get_stats.get_stats()
+    dataframe = pandas.read_csv("stats_data.csv")
+
+    X = dataframe.drop("MVP-Votes", 1)  # Feature Matrix
+    y = dataframe["MVP-Votes"]  # Target Variable
+    # pre-processing
+    cor = dataframe.corr()
+    cor_target = abs(cor["MVP-Votes"])
+    relevant_features = cor_target[cor_target > 0.1]
+    print(relevant_features)
 
 
 if __name__ == '__main__':
